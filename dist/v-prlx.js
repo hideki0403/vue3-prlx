60891f83a0544e97983625f704fe836fa1796786
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-03-18 01:44:07 +0900

    update: Update the artifacts

--- dist/v-prlx.js
+++ dist/v-prlx.js
@@ -1,146 +1,152 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = global || self, factory(global.VuePrlx = {}));
-}(this, function (exports) { 'use strict';
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.VuePrlx = {}));
+})(this, (function (exports) { 'use strict';
 
-  var directive = {
-    bind: onBind,
-    update: onBind,
-    unbind: onUnbind
-  };
-
-  function onUnbind(el) {
-    window.cancelAnimationFrame(el.__prlxRequestAnimationFrameId);
-    delete el.__prlxRequestAnimationFrameId;
-  }
-
-  function onBind(el, _ref) {
-    var _ref$modifiers = _ref.modifiers,
-        modifiers = _ref$modifiers === void 0 ? {} : _ref$modifiers,
-        _ref$value = _ref.value,
-        value = _ref$value === void 0 ? {} : _ref$value;
-    var settings = {
-      isParallaxOnMobile: modifiers.mobile || false,
-      background: modifiers.background || false,
-      startParallaxFromBottom: value.fromBottom || false,
-      justAddParallaxValue: value.custom || false,
-      reverse: value.reverse || false,
-      speed: value.speed || 0.15,
-      preserveInitialPosition: value.preserveInitialPosition === false ? value.preserveInitialPosition : true,
-      direction: value.direction || 'y',
-      limit: value.limit || null,
-      mobileMaxWidth: value.mobileMaxWidth || 768,
-      isDisabled: value.disabled || false
+    // VUE DIRECTIVE DEFINITION
+    const directive = {
+        beforeMount: onBind,
+        updated: onBind,
+        unmounted: onUnbind
+    };
+    // FUNCTIONS
+    function onUnbind(el) {
+        if (!el.__prlxRequestAnimationFrameId)
+            return;
+        window.cancelAnimationFrame(el.__prlxRequestAnimationFrameId);
+        delete el.__prlxRequestAnimationFrameId;
+    }
+    function onBind(el, { modifiers = {}, value = {} }) {
+        // SETUP SETTING
+        const settings = {
+            // {boolean} – enable parallax on mobile
+            isParallaxOnMobile: modifiers.mobile || false,
+            // {boolean} – animate background-position instead of translate
+            background: modifiers.background || false,
+            // {boolean} – start parallax from very bottom of the page instead of middle
+            startParallaxFromBottom: value.fromBottom || false,
+            // {boolean} – just add '--parallax-value' css variable to element for your custom animations
+            // so add css for example: transform: scale(calc(var(--parallax-value) / 50 ))
+            justAddParallaxValue: value.custom || false,
+            // {boolean} – reverse direction
+            reverse: value.reverse || false,
+            // {number} – parallax power
+            speed: value.speed || 0.15,
+            // {boolean} – can parallax to negative values
+            preserveInitialPosition: value.preserveInitialPosition === false
+                ? value.preserveInitialPosition
+                : true,
+            // {string} – 'x' - horizontal parallax, 'y' - vertical
+            direction: value.direction || 'y',
+            // {object} – limit.min, limit.max offset
+            limit: value.limit || null,
+            // {number} – mobile max width
+            mobileMaxWidth: value.mobileMaxWidth || 768,
+            // {boolean} – is parallax disabled
+            isDisabled: value.disabled || false
+        };
+        // DEFAULT SETTINGS FOR BACKGROUND-POSITION
+        if (settings.background) {
+            settings.speed = value.speed || 0.02;
+            settings.limit = {
+                min: 0,
+                max: 100
+            };
+        }
+        // REVERSE DIRECTION
+        if (settings.reverse) {
+            settings.speed = -settings.speed;
+        }
+        if (settings.isDisabled) {
+            onUnbind(el);
+        }
+        else {
+            const isMobile = window.innerWidth < settings.mobileMaxWidth;
+            const shouldParallax = isMobile
+                ? settings.isParallaxOnMobile
+                : true;
+            if (shouldParallax) {
+                init(el, settings);
+            }
+        }
+    }
+    function init(el, settings) {
+        // START PARALLAX FROM MIDDLE OR BOTTOM OF THE SCREEN
+        const startingPoint = settings.startParallaxFromBottom
+            ? window.innerHeight
+            : (window.innerHeight / 2);
+        const pageYOffset = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop);
+        let scrollPosition = pageYOffset - offsetTopFromWindow(el) + startingPoint;
+        // DON'T PARALLAX TO NEGATIVE VALUES (START PARALLAX FROM INITIAL DOM POSITION)
+        if (settings.preserveInitialPosition) {
+            if (scrollPosition < 0)
+                scrollPosition = 0;
+        }
+        // PARALLAX ONLY IN VIEWPORT
+        if (isInViewport(el)) {
+            animate(el, scrollPosition, settings);
+        }
+        el.__prlxRequestAnimationFrameId = window.requestAnimationFrame(init.bind(null, el, settings));
+    }
+    function animate(el, scrollPosition, settings) {
+        let offset = scrollPosition * settings.speed;
+        // NORMALIZE OFFSET
+        if (settings.limit) {
+            if (offset > settings.limit.max)
+                offset = settings.limit.max;
+            if (offset < settings.limit.min)
+                offset = settings.limit.min;
+        }
+        // RUN PARALLAX
+        let parallaxType;
+        if (settings.background) {
+            parallaxType = parallaxBackgroundPosition;
+        }
+        else if (settings.justAddParallaxValue) {
+            parallaxType = addParallaxValueAsCssVariable;
+        }
+        else {
+            parallaxType = parallaxTransform;
+        }
+        parallaxType(el, offset, settings.direction);
+    }
+    function parallaxBackgroundPosition(el, offset, direction) {
+        el.style.transition = `background-position 0.1s ease-out`;
+        if (direction === 'y') {
+            el.style.backgroundPosition = `50% ${offset}%`;
+        }
+        else {
+            el.style.backgroundPosition = `${offset}% 50%`;
+        }
+    }
+    function parallaxTransform(el, offset, direction) {
+        el.style.transition = `transform 0.1s ease-out`;
+        el.style.transform = `translate${direction.toUpperCase()}(${Math.round(offset)}px)`;
+    }
+    function addParallaxValueAsCssVariable(el, offset) {
+        el.style.setProperty('--parallax-value', offset.toString());
+    }
+    const isInViewport = (el, { top: t, height: h } = el.getBoundingClientRect()) => t <= innerHeight && t + h > 0;
+    const offsetTopFromWindow = (element) => {
+        let top = 0;
+        do {
+            top += element.offsetTop || 0;
+            element = element.offsetParent;
+        } while (element);
+        return top;
     };
 
-    if (settings.background) {
-      settings.speed = value.speed || 0.02;
-      settings.limit = {
-        min: 0,
-        max: 100
-      };
-    }
+    const plugin = {
+        install: (app) => {
+            app.directive('prlx', directive);
+        }
+    };
 
-    if (settings.reverse) {
-      settings.speed = -settings.speed;
-    }
+    exports.VuePrlx = plugin;
+    exports.VuePrlxDirective = directive;
+    exports.default = plugin;
 
-    if (settings.isDisabled) {
-      onUnbind(el);
-    } else {
-      var isMobile = window.innerWidth < settings.mobileMaxWidth;
-      var shouldParallax = isMobile ? settings.isParallaxOnMobile : true;
-
-      if (shouldParallax) {
-        init(el, settings);
-      }
-    }
-  }
-
-  function init(el, settings) {
-    var startingPoint = settings.startParallaxFromBottom ? window.innerHeight : window.innerHeight / 2;
-    var pageYOffset = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop);
-    var scrollPosition = pageYOffset - offsetTopFromWindow(el) + startingPoint;
-
-    if (settings.preserveInitialPosition) {
-      if (scrollPosition < 0) scrollPosition = 0;
-    }
-
-    if (isInViewport(el)) {
-      animate(el, scrollPosition, settings);
-    }
-
-    el.__prlxRequestAnimationFrameId = window.requestAnimationFrame(init.bind(null, el, settings));
-  }
-
-  function animate(el, scrollPosition, settings) {
-    var offset = scrollPosition * settings.speed;
-
-    if (settings.limit) {
-      if (offset > settings.limit.max) offset = settings.limit.max;
-      if (offset < settings.limit.min) offset = settings.limit.min;
-    }
-
-    var parallaxType;
-
-    if (settings.background) {
-      parallaxType = parallaxBackgroundPosition;
-    } else if (settings.justAddParallaxValue) {
-      parallaxType = addParallaxValueAsCssVariable;
-    } else {
-      parallaxType = parallaxTransform;
-    }
-
-    parallaxType(el, offset, settings.direction);
-  }
-
-  function parallaxBackgroundPosition(el, offset, direction) {
-    el.style.transition = "background-position 0.1s ease-out";
-
-    if (direction === 'y') {
-      el.style.backgroundPosition = "50% ".concat(offset, "%");
-    } else {
-      el.style.backgroundPosition = "".concat(offset, "% 50%");
-    }
-  }
-
-  function parallaxTransform(el, offset, direction) {
-    el.style.transition = "transform 0.1s ease-out";
-    el.style.transform = "translate".concat(direction.toUpperCase(), "(").concat(Math.round(offset), "px)");
-  }
-
-  function addParallaxValueAsCssVariable(el, offset) {
-    el.style.setProperty('--parallax-value', offset);
-  }
-
-  var isInViewport = function isInViewport(el) {
-    var _ref2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : el.getBoundingClientRect(),
-        t = _ref2.top,
-        h = _ref2.height;
-
-    return t <= innerHeight && t + h > 0;
-  };
-
-  var offsetTopFromWindow = function offsetTopFromWindow(element) {
-    var top = 0;
-
-    do {
-      top += element.offsetTop || 0;
-      element = element.offsetParent;
-    } while (element);
-
-    return top;
-  };
-
-  var _plugin = (function (Vue) {
-    Vue.directive('prlx', directive);
-  });
-
-  exports.VuePrlx = _plugin;
-  exports.VuePrlxDirective = directive;
-  exports.default = _plugin;
-
-  Object.defineProperty(exports, '__esModule', { value: true });
+    Object.defineProperty(exports, '__esModule', { value: true });
 
 }));
