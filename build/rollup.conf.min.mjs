5c2fcc7411f1543a9b12ad056587d29be7ecd77e
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-03-18 01:34:58 +0900

    feat: Add TypeScript build support

--- build/rollup.conf.min.mjs
+++ build/rollup.conf.min.mjs
@@ -1,11 +1,13 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
 
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   plugins: [
+    typescript(),
     nodeResolve(),
     babel(),
     terser(), // uglifyjs alternative with es6 support
--- build/rollup.conf.mjs
+++ build/rollup.conf.mjs
@@ -1,9 +1,11 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
+import typescript from '@rollup/plugin-typescript'
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   plugins: [
+    typescript(),
     nodeResolve(),
     babel(),
   ],
